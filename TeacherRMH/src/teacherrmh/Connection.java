303a4a7ca95c9e004f0d9499a80559cdac6a79c1
jmuller4 <jmuller4@IST-TECH-108-16.ccsd.d5>
2014-02-26 16:45:49 -0700

    Revert "Revert "Revert "Revert "Removed a System.out.println""""
    This reverts commit a76ddf64a820098cfc0eadc56e05b99509a48aa7.

--- TeacherRMH/src/teacherrmh/Connection.java
+++ TeacherRMH/src/teacherrmh/Connection.java
@@ -72,7 +72,6 @@ public class Connection {
                         System.out.println("DISCONNECTED");
                     }
                     //System.out.println("1st check: " + MainClass.textField.getText().indexOf(username));
-                    System.out.println("Thread running");
                     if (MainClass.removeFirstInLine) {
                         System.out.println("Remove first");
                         if (MainClass.textField.getText().indexOf(username) == 4) {
